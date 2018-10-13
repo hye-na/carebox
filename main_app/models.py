c6c79408cdbf06bf9a01a56c50973bc9491d95cc
Hannah Park <hye.na.pa@gmail.com>
2018-10-12 17:05:07 -0700

    create models

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+CATEGORIES = (
+    ('S', 'Snacks'),
+    ('P', 'Personal Care'),
+    ('G', 'Gift Cards'),
+    ('H', 'Home Goods'),
+    ('A', 'Accessories')
+)
 
-class Order(models.Model):
-    recipient = models.CharField(max_length=60)
+
+class Recipient(models.Model):
+    name = models.CharField(max_length=60)
     street = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
@@ -13,5 +21,29 @@ class Order(models.Model):
     phone = models.CharField(max_length=11)
     email = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
-# class Product(models.Model):
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=250)
+    price = models.IntegerField()
+    category = models.CharField(
+        max_length=1,
+        choices=CATEGORIES,
+        default='S',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(Recipient, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
